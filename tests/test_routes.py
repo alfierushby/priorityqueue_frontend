db826c615fee1343e791a6c5b2274a6963db8569
ickeyben123 <rushbyalfie99@gmail.com>
2025-02-19 13:39:54 +0000

    Add pytest CI/CD

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -38,88 +38,88 @@ def test_medium_priority_post(client):
     general_priority_post(client, "Medium")
 
 
-# def test_low_priority_post(client):
-#     """Test posting a low priority request with form data
-#     :param client: The client to interact with the app
-#     """
-#     general_priority_post(client, "Low")
-#
-#
-# def test_high_priority_post(client):
-#     """Test posting a high priority request with form data
-#     :param client: The client to interact with the app
-#     """
-#     general_priority_post(client, "High")
-#
-#
-# def test_empty_string_description_post(client):
-#     """Test a post with an empty string description
-#     :param client: The client to interact with the app
-#     """
-#     # Simulate form submission
-#     response = client.post("/api/priority/", data={
-#         "title": "Urgent Issue",
-#         "description": "",
-#         "priority": "Medium"
-#     }, content_type="application/x-www-form-urlencoded")
-#
-#     assert response.status_code == 400
-#
-#     error_data = response.get_json()
-#
-#     assert error_data["error_type"] == "validation_error"
-#     expected_error = {
-#         "field": ["description"],
-#         "message": "String should have at least 1 character"
-#     }
-#     assert expected_error in error_data["details"]
-#
-#
-# def test_null_description_post(client):
-#     """Test a post with a null description
-#     :param client: The client to interact with the app
-#     """
-#     # Simulate form submission
-#     response = client.post("/api/priority/", data={
-#         "title": "Urgent Issue",
-#         "priority": "Medium"
-#     }, content_type="application/x-www-form-urlencoded")
-#
-#     assert response.status_code == 400
-#
-#     error_data = response.get_json()
-#
-#     assert error_data["error_type"] == "validation_error"
-#     expected_error = {
-#         "field": ["description"],
-#         "message": "Input should be a valid string"
-#     }
-#     assert expected_error in error_data["details"]
-#
-#
-# def test_null_post(client):
-#     """Test a post with a null body
-#     :param client: The client to interact with the app
-#     """
-#     # Simulate form submission
-#     response = client.post("/api/priority/", content_type="application/x-www-form-urlencoded")
-#
-#     assert response.status_code == 400
-#
-#     error_data = response.get_json()
-#
-#     assert error_data["error_type"] == "validation_error"
-#     expected_errors = [{
-#         "field": ["description"],
-#         "message": "Input should be a valid string"
-#     },
-#         {
-#             "field": ["title"],
-#             "message": "Input should be a valid string"
-#         },
-#         {
-#             "field": ["priority"],
-#             "message": "Input should be a valid string"
-#         }
-#     ]
-#     for expected_error in expected_errors: assert expected_error in error_data["details"]
+def test_low_priority_post(client):
+    """Test posting a low priority request with form data
+    :param client: The client to interact with the app
+    """
+    general_priority_post(client, "Low")
+
+
+def test_high_priority_post(client):
+    """Test posting a high priority request with form data
+    :param client: The client to interact with the app
+    """
+    general_priority_post(client, "High")
+
+
+def test_empty_string_description_post(client):
+    """Test a post with an empty string description
+    :param client: The client to interact with the app
+    """
+    # Simulate form submission
+    response = client.post("/api/priority/", data={
+        "title": "Urgent Issue",
+        "description": "",
+        "priority": "Medium"
+    }, content_type="application/x-www-form-urlencoded")
+
+    assert response.status_code == 400
+
+    error_data = response.get_json()
+
+    assert error_data["error_type"] == "validation_error"
+    expected_error = {
+        "field": ["description"],
+        "message": "String should have at least 1 character"
+    }
+    assert expected_error in error_data["details"]
+
+
+def test_null_description_post(client):
+    """Test a post with a null description
+    :param client: The client to interact with the app
+    """
+    # Simulate form submission
+    response = client.post("/api/priority/", data={
+        "title": "Urgent Issue",
+        "priority": "Medium"
+    }, content_type="application/x-www-form-urlencoded")
+
+    assert response.status_code == 400
+
+    error_data = response.get_json()
+
+    assert error_data["error_type"] == "validation_error"
+    expected_error = {
+        "field": ["description"],
+        "message": "Input should be a valid string"
+    }
+    assert expected_error in error_data["details"]
+
+
+def test_null_post(client):
+    """Test a post with a null body
+    :param client: The client to interact with the app
+    """
+    # Simulate form submission
+    response = client.post("/api/priority/", content_type="application/x-www-form-urlencoded")
+
+    assert response.status_code == 400
+
+    error_data = response.get_json()
+
+    assert error_data["error_type"] == "validation_error"
+    expected_errors = [{
+        "field": ["description"],
+        "message": "Input should be a valid string"
+    },
+        {
+            "field": ["title"],
+            "message": "Input should be a valid string"
+        },
+        {
+            "field": ["priority"],
+            "message": "Input should be a valid string"
+        }
+    ]
+    for expected_error in expected_errors: assert expected_error in error_data["details"]
